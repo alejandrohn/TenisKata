4f32f3d3bb58bc25a0a53d418608c859908b0718
Alejandro Herrera Nogueira <Aherrera@innovartecnologias.com>
2021-07-02 21:30:54 +0200

    Se agrega la interfaz Point, todavía no se ha adaptado a la primera lógica implementada, v1

---
+++ TenisKata/IPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenisKata
+{
+    interface IPoint
+    {
+        public bool isGamePoint();
+
+        public int GetPointValue();
+
+        public void SetWinPlayer(IPlayer player);
+
+        public void SetDefeatPlayer(IPlayer player);
+
+        public IPlayer GetWinPlayer();
+
+        public IPlayer GetDefeatPlayer();
+
+    }
+}
