414d5f78b7819113b1d9f8982425c714b3618212
Alejandro Herrera Nogueira <Aherrera@innovartecnologias.com>
2021-07-05 20:51:54 +0200

    Se crea un proyecto para hacer unit testing

---
+++ UnitTestKataTenis/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestKataTenis
+{
+    [TestClass]
+    public class TenisKataTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
