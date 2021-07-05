b4ed6cf97bdbb3b6b755d2025635bdd26dab3acf
Alejandro Herrera Nogueira <Aherrera@innovartecnologias.com>
2021-07-05 20:51:24 +0200

    Invocación para empezar el partido, llamado a la funcionalidad de estadísticas

--- TenisKata/Program.cs
+++ TenisKata/Program.cs
@@ -7,17 +7,26 @@ namespace TenisKata
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            PlayMatch();
         }
 
         static void PlayMatch()
         {
-            Player playerA = new Player();
-            Player playerB = new Player();
+            Player playerA = new Player("Player1");
+            Player playerB = new Player("Player2");
 
             Match match = new Match(playerA, playerB);
 
+            match.Start();
+           
+            Stadistics stadistics = new Stadistics(match);
 
+            stadistics.PrintJugadorGanador();
+            stadistics.PrintStadisticsPlayer1();
+            stadistics.PrintStadisticsPlayer2();
+            stadistics.PrintStadisticsSets();
 
+            Console.ReadLine();
 
         }
     }
